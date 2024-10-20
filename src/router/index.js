010949569222cbcc5edd6bdb61583a438f9abb3c
Mohammad Baraa Almsouti <baraasy55@gmail.com>
2024-10-19 18:17:23 -0700

    Full Vue-Jobs Project

---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import Homeview from '@/views/Homeview.vue';
+import JobsViwe from '@/views/JobsView.vue';
+import NotFoundView from '@/views/NotFoundView.vue';
+import AddjobView from '@/views/AddjobView.vue';
+import JobView from '@/views/JobView.vue';
+import EditeJobView from '@/views/EditJobView.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Homeview,
+    },
+    {
+      path: '/jobs',
+      name: 'job',
+      component: JobsViwe,
+    },
+    {
+      path: '/jobs/:id',
+      name: 'jobs',
+      component: JobView,
+    },
+    {
+      path: '/jobs/add',
+      name: 'add-job',
+      component: AddjobView,
+    },
+    {
+      path: '/jobs/edit/:id',
+      name: 'edit-job',
+      component: EditeJobView,
+    },
+    {
+      path: '/:catchAll(.*)', // that's mean all the files that not found
+      name: 'not-found',
+      component: NotFoundView,
+    },
+  ],
+});
+
+export default router;
